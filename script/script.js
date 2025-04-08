0a48b6f7ffe036bdc4c96f03eab2f861bdf79f83
patelpranay102 <pranayhareshkumar.patel@edu.sait.ca>
2025-04-08 12:09:07 -0600

    Flex box element changes done
    some changes in code

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function showmessage()
+{
+    let age =19;
+    let Name = "James";
+    if (age >= 18)
+    {
+        alert("Hello" + Name + "You can now Drive :);")
+    }
+    else
+    {
+        alert("Hello Kid," + "You are not young to Drive")
+    }
+}
